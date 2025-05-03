5554eda28b53016866d86cd1fa1a7e7f0e706305
Faizan Alam <technicalalam803@gmail.com>
2025-05-03 16:22:07 +0530

    fixed product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,7 +4,7 @@ const productController = require("../controllers/products.controller");
 const { authenticateToken } = require("../middleware/authMiddleware")
 
 router.get("/collections", authenticateToken, productController.getAllCategories);
-router.get("/products", authenticateToken, productController.getAllProducts);
+router.get("/products", productController.getAllProducts);
 router.get("/productsById", productController.getProductById);
 router.post("/createProduct", authenticateToken, productController.createProduct);
 router.put("/updateProduct", authenticateToken, productController.updateProduct);
