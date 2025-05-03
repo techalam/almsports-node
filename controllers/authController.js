9fe200a0a86bc55349eedab9a9854e651e41d519
Faizan Alam <technicalalam803@gmail.com>
2025-05-03 12:59:42 +0530

    updated bcrypt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 require("dotenv").config();
